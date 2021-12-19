2f1409df48a4139c9e86632017527baf17f13649
Hyuk Jin Choi <sc62070@gmail.com>
2021-12-19 19:24:12 +0900

    Update main.js

--- casitacnft/main.js
+++ casitacnft/main.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("Dec 22, 2021 01:00:00").getTime();
+let launchDate = new Date("Dec 23, 2021 01:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
